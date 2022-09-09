d1feb1e46c9b31d3cd6a8902f386630eb70cb4e6
Behnam <behnamsh72@gmail.com>
2022-09-09 14:01:56 +0430

    create a json response

--- api/views.py
+++ api/views.py
@@ -3,13 +3,19 @@ import json
 
 
 def api_home(request, *args, **kwargs):
+    print(request.GET) #url query params
+    print(request.POST)
     body = request.body  # byte string of JSON Data
     data = {}
     try:
-        data = json.loads(body) # String of json data--> Python Dict
+        data = json.loads(body)  # String of json data--> Python Dict
     except:
         pass
 
     print(data.keys())
     print(body)
+    data['params']=dict(request.GET)
+    data['headers'] = dict(request.headers) # request.META
+    print(request.headers)
+    data['content_type'] = request.content_type
     return JsonResponse(data)
