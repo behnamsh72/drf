02e57b257f0362705306d901536e03a0d485281b
Behnam <behnamsh72@gmail.com>
2022-12-11 21:43:34 +0330

    save data with serializer and before django restframework generics

--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
 from django.forms.models import model_to_dict
+
 from products.models import Product
+from products.serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
 
 # def api_home(request, *args, **kwargs):
 #     print(request.GET) #url query params
@@ -20,16 +25,86 @@ from products.models import Product
 #     print(request.headers)
 #     data['content_type'] = request.content_type
 #     return JsonResponse(data)
+
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if model_data:
+#         data['id']=model_data.id
+#         data['title'] = model_data.title
+#         data['content']=model_data.content
+#         data['price']=model_data.price
+#         # model instance(model_data)
+#         # turn  a Python dict
+#         # return json to my client
+#         # this process called Serialization
+#     return JsonResponse(data)
+
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if model_data:
+#         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+#         print(data)
+#         json_data_str=json.dumps(data)
+#     return HttpResponse(json_data_str,headers={"content-type":"application/json"})
+#
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if model_data:
+#         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+#     return JsonResponse(data)
+
+##Using Response instead of JsonResponse
+##change model_to_dict to a Custom Serializar
+# @api_view(["GET", "POST"])
+
+# @api_view(["GET"])
+# def api_home(request, *args, **kwargs):
+#     """ DRF API VIEW"""
+#     #
+#     # if request.method != "POST":
+#     #     return Response({"detail": "GET NOT ALLOwed"}, status=405)
+#     instance = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if instance:
+#         data=ProductSerializer(instance).data
+#     return Response(data)
+
+# @api_view(["POST"])
+# def api_home(request, *args, **kwargs):
+#     """ DRF API VIEW"""
+#     #
+#     # if request.method != "POST":
+#     #     return Response({"detail": "GET NOT ALLOwed"}, status=405)
+#     data=request.data
+#     instance = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if instance:
+#         data=ProductSerializer(instance).data
+#     return Response(data)
+#
+
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data['id']=model_data.id
-        data['title'] = model_data.title
-        data['content']=model_data.content
-        data['price']=model_data.price
-        # model instance(model_data)
-        # turn  a Python dict
-        # return json to my client
-        # this process called Serialization
-    return JsonResponse(data)
+    """
+    DRF API VIEW
+    :param request:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    data = request.data
+    serializer=ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        instance=serializer.save()
+        #instance=form.save()
+        print(serializer.data)
+        data=serializer.data
+        return Response(serializer.data)
+    return Response({"invalid":"not good data"},status=400)
+
--- products/models.py
+++ products/models.py
@@ -6,4 +6,16 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=15, decimal_places=2,default=99.99)
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+
+    @property
+    def sale_price(self):
+        print(type(self.price))
+
+        x = float(self.price)
+        print(type(x))
+        num = x * 0.8
+        return "%.2f" % num
+    @property
+    def get_discount(self):
+        return "122"
