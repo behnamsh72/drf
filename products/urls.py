f055826b250e69e9e9fe62a9bfad299be92a5e93
Behnam <behnamsh72@gmail.com>
2022-12-14 20:29:27 +0330

    back to generics list create view and detail api view

--- products/urls.py
+++ products/urls.py
@@ -7,16 +7,16 @@ urlpatterns=[
 
     # path('',views.product_create_view),
 
-    # path('',views.product_list_create_view),
+    path('',views.product_list_create_view),
 
-    #Primary key (pk)
-    # path('<int:pk>/',views.ProductDetailAPIView.as_view())
+    # Primary key (pk)
+    path('<int:pk>/',views.ProductDetailAPIView.as_view())
     #ANother method of implementing
     # path('<int:pk>/', views.ProductDetailAPIView)
 
 
     #Just for all in one method we implemented in this way:
-    path('',views.product_alt_view),
-    path('<int:pk>/',views.product_alt_view)
+    # path('',views.product_alt_view),
+    # path('<int:pk>/',views.product_alt_view)
 
 ]
