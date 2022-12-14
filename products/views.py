8e611f01203df090494da498d3a079b44e46a254
Behnam <behnamsh72@gmail.com>
2022-12-14 20:27:55 +0330

    instead of generics we use function with header api view in products view but is dirty

--- products/views.py
+++ products/views.py
@@ -2,6 +2,11 @@ from rest_framework import generics
 
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from django.http import Http404
+
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -26,41 +31,77 @@ from .serializers import ProductSerializer
 #
 # product_create_view=ProductCreateApiView.as_view()
 
+# if we use get method this function retriev all the products and
+# if we use post method this function creates all the
 class ProductListCreateApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def perform_create(self, serializer):
-        #serializer.save(user=self.request.user)
+        # serializer.save(user=self.request.user)
         print(serializer.validated_data)
-        title=serializer.validated_data.get('title')
-        content=serializer.validated_data.get('title')
-        #or None
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('title')
+        # or None
         if content is None:
-            content=title
+            content = title
 
         serializer.save(content=content)
-        #Send  a Django signal
+        # Send  a Django signal
 
 
-product_list_create_view=ProductListCreateApiView.as_view()
+product_list_create_view = ProductListCreateApiView.as_view()
+
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk'
-    #Product.ob
-#for second method
+    # Product.ob
+
+
+# for second method
 # product_detail_view=ProductDetailAPIView.as_view()
 
 
 class ProductListApiView(generics.ListAPIView):
-
     """
     Not gonna use this method
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    #lookup_field='pk' ?
+    # lookup_field='pk' ?
 
-product_list_view=ProductListApiView.as_view()
+
+product_list_view = ProductListApiView.as_view()
+
+
+# All In One
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+    if method == "GET":
+        if pk is not None:
+            # details view
+            # queryset = Product.objects.filter(pk=pk)
+            # if not queryset.exist():
+            #     raise Http404
+            # or we can use this method
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        else:
+            queryset = Product.objects.all()
+            data = ProductSerializer(queryset, many=True).data
+            return Response(data)
+    if method == "POST":
+        # create an item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
