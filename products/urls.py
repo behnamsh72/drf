bf5a272e1ca5ebf16e4c00e32743075b69581967
Behnam <behnamsh72@gmail.com>
2022-12-26 01:12:01 +0330

    end of urls,Reverse & Serializer

--- products/urls.py
+++ products/urls.py
@@ -8,14 +8,14 @@ urlpatterns=[
     # path('',views.product_create_view),
 
 
-    path('',views.product_list_create_view),
+    path('',views.product_list_create_view,name='product-list'),
 
 
-    path('<int:pk>/update', views.ProductUpdateAPIView.as_view()),
+    path('<int:pk>/update', views.ProductUpdateAPIView.as_view(),name="product-edit"),
     path('<int:pk>/delete', views.ProductDeleteAPIView.as_view()),
 
     # Primary key (pk)
-    path('<int:pk>/',views.ProductDetailAPIView.as_view())
+    path('<int:pk>/',views.ProductDetailAPIView.as_view(),name='product-detail')
     #ANother method of implementing this
     # path('<int:pk>/', views.ProductDetailAPIView)
 
