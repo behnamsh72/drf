6fcff923de8e78a12627c0670abf05d6537d710f
Behnam <behnamsh72@gmail.com>
2022-12-27 02:25:20 +0330

    use validators in serializer

--- products/serializers.py
+++ products/serializers.py
@@ -2,14 +2,29 @@ from rest_framework import serializers
 from .models import Product
 from rest_framework.reverse import reverse
 
+from . import validators
 class ProductSerializer(serializers.ModelSerializer):
-    my_discount=serializers.SerializerMethodField(read_only=True)
-    edit_url=serializers.SerializerMethodField(read_only=True)
-    url=serializers.HyperlinkedIdentityField(view_name='product-detail',lookup_field='pk')
+    my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
+    title=serializers.CharField(validators=[validators.validate_title_no_hello , validators.unique_product_title])
+    # name=serializers.CharField(source='title',read_only=True)
     class Meta:
         model = Product
         # fields = ['title', 'content', 'price', 'sale_price', 'get_discount']
-        fields = ['pk','url','edit_url','title', 'content', 'price', 'sale_price', 'my_discount']
+        fields = ['pk', 'url', 'edit_url', 'title','content', 'price', 'sale_price', 'my_discount']
+
+        fields = ['pk', 'url', 'edit_url', 'title','content', 'price', 'sale_price', 'my_discount']
+        # fields = ['pk', 'url', 'edit_url', 'title','name','content', 'price', 'sale_price', 'my_discount']
+
+    #
+    # def validate_title(self, value):
+    #     requests=self.context.get('request')
+    #     user=requests.user
+    #     qs=Product.objects.filter(user=user,title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
 
     # def get_my_discount(self,obj):
     #     try:
@@ -30,17 +45,16 @@ class ProductSerializer(serializers.ModelSerializer):
     #     return super().update(instance,validated_data)
     #
 
-
-    def get_edit_url(self,obj):
+    def get_edit_url(self, obj):
         # return f"/api/products/{obj.pk}/"
-        request=self.context.get('request')
+        request = self.context.get('request')
         if request is None:
             return None
-        return reverse("product-edit",kwargs={"pk":obj.pk},request=request)
-    def get_my_discount(self,obj):
-        if not hasattr(obj,'id'):
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
+
+    def get_my_discount(self, obj):
+        if not hasattr(obj, 'id'):
             return None
-        if not isinstance(obj,Product):
+        if not isinstance(obj, Product):
             return None
         return obj.get_discount
-
