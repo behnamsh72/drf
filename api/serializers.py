d8870744b8d6aba0ccec3617fa601bc3e91faf77
Behnam <behnamsh72@gmail.com>
2022-12-27 21:57:33 +0330

    using modelSerializer instad Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,23 +1,43 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
-#We import entire module to avoid circular Import problem
+User = get_user_model()
+
+# We import entire module to avoid circular Import problem
 # from products.serializers import ProductSerializer
 import products.serializers
 
 
-class UserProductInlineSerializer(serializers.Serializer):
-    url=serializers.HyperlinkedIdentityField(view_name='product-detail',lookup_field='pk',read_only=True)
-    title=serializers.CharField(read_only=True)
+class UserProductInlineSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk', read_only=True)
+    title = serializers.CharField(read_only=True)
+
 
 class UserPublicSerializer(serializers.Serializer):
-    username=serializers.CharField(read_only=True)
-    id=serializers.IntegerField(read_only=True)
-    other_products=serializers.SerializerMethodField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    id = serializers.IntegerField(read_only=True)
+    this_is_not_real = serializers.CharField(read_only=True)
 
-    def get_other_products(self,obj):
-        # request=self.context.get('request')
-        # print(obj)
-        user=obj
-        #only five products
-        my_products_qs=user.product_set.all()[:5]
-        return UserProductInlineSerializer(my_products_qs,many=True,context=self.context).data
+    # other_products=serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'this_is_not_real',
+            'id'
+        ]
+
+    def create(self, validated_data):
+        user=User.objects.create(**validated_data)
+        return super().create(self, validated_data)
+
+    def update(self, instance, validated_data):
+        return super().update(self, instance, validated_data)
+    # def get_other_products(self,obj):
+    #     # request=self.context.get('request')
+    #     # print(obj)
+    #     user=obj
+    #     #only five products
+    #     my_products_qs=user.product_set.all()[:5]
+    #     return UserProductInlineSerializer(my_products_qs,many=True,context=self.context).data
